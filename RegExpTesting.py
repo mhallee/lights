16c1e6b38fe9c1a1f6bf22e19755db62ca7198b7
Piano <piano@nat-oitwireless-inside-vapornet100-c-15713.Princeton.EDU>
2018-07-20 11:39:26 -0400

    Finally fixed regexp

--- RegExpTesting.py
+++ RegExpTesting.py
@@ -51,7 +51,7 @@ print "***************************"
 cdBrickText = cdBrickFile.read()
 
 #Extract vertex points on base
-pattern = re.compile("Vertices:(\r?\n)(.*?);",re.DOTALL)
+pattern = re.compile("Vertices:\r?\n(.*?);",re.DOTALL)
 baseMatches = re.findall(pattern,cdBaseText)
 print "base match count" + str(len(baseMatches))
 print "basematches [0] type" + str(type(baseMatches[0]))
